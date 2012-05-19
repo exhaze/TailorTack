2cb61f75260dbe53ab5ec9f74b957bdeca23f69f
Eugene Yaroslavtsev <eugene@luidia.com>
2012-05-18 19:35:45 -0700

    created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TailorTack::Application.config.secret_token = '7dc036e11298cb38449911890d9182fc2443125549a25a3736af2a4ccaf082e28915409374edc9e68bded03365290b91a9754b0171011b37dfa2d6c91e32ed5f'
