62bdc97f22a1538489b7d3a9be0cfb414bbe50aa
Eugene Yaroslavtsev <exhaze@gmail.com>
2012-08-10 20:37:14 -0700

    - updated gems - added customers schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120519024446) do
+ActiveRecord::Schema.define(:version => 20120519031107) do
+
+  create_table "customers", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "orders", :force => true do |t|
     t.decimal  "price"
